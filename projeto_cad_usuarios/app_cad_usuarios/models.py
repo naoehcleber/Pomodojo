528b0f78bfe916f89f285798dd03d005ebc21bb6
Anabia <ana.00000850038@unicap.br>
2024-10-18 16:32:54 -0300

    Adicao na interface de opcao de musicas para o pomodoro

--- projeto_cad_usuarios/app_cad_usuarios/models.py
+++ projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-# class Usuario(models.Model):
-#     id_usuario = models.AutoField(primary_key = True)
-#     nome = models.TextField(max_length =255)
-#     idade = models.IntegerField()
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key = True)
+    nome = models.TextField(max_length =255)
+    idade = models.IntegerField()
 
 class CriarUsuario(models.Model):
     id_usuario = models.AutoField(primary_key = True)

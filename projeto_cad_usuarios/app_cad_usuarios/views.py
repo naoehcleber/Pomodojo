778c3cbd5d95cec687bd402ac5c779fbe25469f2
Joao Victor <joao.vr.fernandes@gmail.com>
2024-10-19 15:37:34 -0300

    iniciado formulario de cadastro

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,27 +1,39 @@
-from django.shortcuts import render
-from .models import Usuario
-# Create your views here.
-def home(requests):
-    return render(requests, 'usuarios/usuarioPage.html')
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import inlineformset_factory
+from .models import *
+from .forms import OrderForm, CreateUserForm
 
-def usuarios(requests):
+# Create your views here.
+def home(request):
+    return render(request, 'usuarios/usuarioPage.html')
+
+def usuarios(request):
     novo_usuario = Usuario()
-    novo_usuario.nome = requests.POST.get('nome')
-    novo_usuario.idade = requests.POST.get('idade')
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
     #exibir todos os usuarios ja cadstrados em uma nova pagina
     usuarios = {
         'usuarios'  : Usuario.objects.all()
     }
     #retorna os dados para a pagina de listagem de usuarios
-    return render(requests, 'usuarios/usuarios.html', usuarios)
+    return render(request, 'usuarios/usuarios.html', usuarios)
 
-def usuarioPage(requests):
-    return render(requests, 'usuarios/usuarioPage.html', usuarioPage)
+def usuarioPage(request):
+    return render(request, 'usuarios/usuarioPage.html', usuarioPage)
 
-def login(requests):
-    return render(requests, 'login/singin_template.html')
+def login(request):
+    return render(request, 'login/singin_template.html')
 
-def logon(requests):
-    return render(requests, 'login/cadastro.html')
+def logon(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("")
+    else:
+        form = CreateUserForm()
+    return render(request, 'login/cadastro.html', {"form": form})
 
--- projeto_cad_usuarios/pomodoro/urls.py
+++ projeto_cad_usuarios/pomodoro/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'pomodoro'
+
 urlpatterns = [
     path('', views.pomodoro_view, name='pomodoro'),
 ]
